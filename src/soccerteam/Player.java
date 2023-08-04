e10e76105408939b0a24749da80fd7a0e7ebdb77
EthanLawyer <zhu.yixiao@outlook.com>
2023-08-04 13:47:09 -0700

    Added the fields and the constructor.

--- src/soccerteam/Player.java
+++ src/soccerteam/Player.java
@@ -1,4 +1,31 @@
 package soccerteam;
 
-public class Player {
+import java.time.LocalDate;
+
+/**
+ * A player in a soccer team.
+ */
+public class Player implements Iplayer{
+  protected String fistName;
+  protected String lastName;
+  protected LocalDate dateOfBirth;
+  Position preferredPosition;
+  int skillLevel;
+  protected int jerseyNumber;
+  Position actualPosition;
+
+  /**
+   * Constructor.
+   */
+  public Player(String fistName, String lastName, LocalDate dateOfBirth,
+            Position preferredPosition, int skillLevel, int jerseyNumber)
+  {
+    this.fistName = fistName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+    this.preferredPosition = preferredPosition;
+    this.skillLevel = skillLevel;
+    this.jerseyNumber = jerseyNumber;
+    this.actualPosition = Position.BENCH;
+  }
 }
