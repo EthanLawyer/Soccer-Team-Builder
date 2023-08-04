6fbb12f0e5886690d8c7b2d2847bf9d0363edeb6
EthanLawyer <zhu.yixiao@outlook.com>
2023-08-04 02:18:15 -0700

    Added the Player enum and defined its toString methods.

--- src/soccerteam/Position.java
+++ src/soccerteam/Position.java
@@ -1,4 +1,37 @@
 package soccerteam;
 
+/**
+ * The position of a player.
+ */
 public enum Position {
+  GOALIE {
+    @Override
+    public String toString() {
+      return "Goalie";
+    }
+  },
+  DEFENDER {
+    @Override
+    public String toString() {
+      return "Defender";
+    }
+  },
+  MIDFIELDER {
+    @Override
+    public String toString() {
+      return "Midfielder";
+    }
+  },
+  FORWARD {
+    @Override
+    public String toString() {
+      return "Forward";
+    }
+  },
+  BENCH {
+    @Override
+    public String toString() {
+      return "Bench";
+    }
+  }
 }
