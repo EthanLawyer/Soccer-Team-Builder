7b21258489709108a7aed70f1d7d01a751963b41
EthanLawyer <zhu.yixiao@outlook.com>
2023-08-04 10:25:28 -0700

    Added the SoccerTeam interface and declared its methods.

--- src/soccerteam/SoccerTeam.java
+++ src/soccerteam/SoccerTeam.java
@@ -1,4 +1,73 @@
 package soccerteam;
 
+/**
+ * A soccer team. A team have a minimum of 10 Players and a maximum of 20.
+ * If there are less than 10 players, the team cannot be created unless more
+ * players are added. If the team has more than 20 players, the ones with
+ * the lowest skill level will be ignored so that we only have 20 players.
+ */
 public interface SoccerTeam {
+  /**
+   * Gets the name of this team.
+   * @return a string
+   */
+  String getName();
+
+
+  /**
+   * Gets the size of this team.
+   * @return int
+   */
+  int getSize();
+
+
+  /**
+   * Returns whether this team has valid number of Players.
+   * @return boolean
+   */
+  boolean isValidTeam();
+
+
+  /**
+   * Generates a random jersey number between 1 and 20.
+   * @return int
+   */
+  int generateJerseyNumber();
+
+
+  /**
+   * Adds a Player to this team.
+   * @param    player    an Iplayer object
+   */
+  void addPlayer(Iplayer player);
+
+
+  /**
+   * Removes a player from this team.
+   * @param   jerseyNumber  the jersey number of the player to be removed
+   */
+  void removePlayer(int jerseyNumber);
+
+
+  /**
+   * Selects the starting lineup of 7 players. The most skilled players
+   * in the team will be selected. If possible, these players will be
+   * assigned their preferred positions, but not necessary. However, the
+   * Goalie always remains the Goalie.
+   */
+  void selectStartingLineup();
+
+
+  /**
+   * Gets all the players on this team.
+   * @return a string
+   */
+  String getAllPlayers();
+
+
+  /**
+   * Gets the starting lineup of this team.
+   * @return a string
+   */
+  String getStartingLineup();
 }
