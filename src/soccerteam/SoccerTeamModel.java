11bc2b71ced5b78cab7113a15d21ff0b5ee2ef50
EthanLawyer <zhu.yixiao@outlook.com>
2023-08-04 18:32:20 -0700

    Added fields and the constructor.

--- src/soccerteam/SoccerTeamModel.java
+++ src/soccerteam/SoccerTeamModel.java
@@ -1,4 +1,54 @@
 package soccerteam;
 
-public class SoccerTeamModel {
+import java.util.HashMap;
+
+public class SoccerTeamModel implements SoccerTeam{
+private String teamName;
+private int teamSize;
+private HashMap<Integer, Iplayer> players;
+
+  /**
+   * Constructor.
+   */
+  public SoccerTeamModel(String teamName) {
+    this.teamName = teamName;
+    this.teamSize = 0;
+    this.players = new HashMap<>();
+  }
+
+  @Override public String getName() {
+    return null;
+  }
+
+  @Override public int getSize() {
+    return 0;
+  }
+
+  @Override public boolean isValidTeam() {
+    return false;
+  }
+
+  @Override public int generateJerseyNumber() {
+    return 0;
+  }
+
+  @Override public void addPlayer(Iplayer player) {
+
+  }
+
+  @Override public void removePlayer(int jerseyNumber) {
+
+  }
+
+  @Override public void selectStartingLineup() {
+
+  }
+
+  @Override public String getAllPlayers() {
+    return null;
+  }
+
+  @Override public String getStartingLineup() {
+    return null;
+  }
 }
